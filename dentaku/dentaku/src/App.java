bc6da2c5cf098f09f28a61274586fcc36e97910b
ryu <ryu140310@icloud.com>
2022-07-08 13:29:49 +0900

    a

--- dentaku/dentaku/src/App.java
+++ dentaku/dentaku/src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        int a=1;
+        System.out.println(a);
     }
 }
